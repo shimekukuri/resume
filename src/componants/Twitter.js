b3dda02d5e9dcae0490913145dbebd8016e37264
shimekukuri <jhutchinson192390@gmail.com>
2022-05-26 14:29:54 -0400

    removed uneeded dependencies

--- src/componants/Twitter.js
+++ src/componants/Twitter.js
@@ -1,10 +1,7 @@
 import React from "react";
-import clsx from "clsx";
 import {TwitterTimelineEmbed} from "react-twitter-embed"
 
 export default function Twitter(props) {    
-
-    const classes = clsx();
 
     return (<>
     <div className="twittercontainer">
