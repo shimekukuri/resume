8bf2772d61f7f21ad508ecb46857eb231f878815
shimekukuri <jhutchinson192390@gmail.com>
2022-05-26 14:29:37 -0400

    Moved all individual state down into it's own componant for each blogpost

--- src/componants/Blog.js
+++ src/componants/Blog.js
@@ -1,33 +1,18 @@
-import react, {useEffect, useState, useLayoutEffect} from "react";
-import clsx from "clsx";
-import {PortableText} from '@portabletext/react'
+import React from "react";
+import Bloglet from "../subcomponants/Bloglet";
 
 
 export default function Blog(props) {
 
     //const names = props.blogList.map((item) => <h1>{item.name}</h1>)
     
-    const renderList = props.blogList;
     console.log(props.blogList)
     
-    const handleTitleClick = (e) => {
-        console.log(e.target)
-    };
-
     return(<>
-    <div id="blogtainer" onClick={handleTitleClick}>
-        {props.blogList.slice(0).sort((a,b) => {
+    <div id="blogtainer">
+        {props.blogList.length > 0 ? props.blogList.slice(0).sort((a,b) => {
             return new Date(b._createdAt) - new Date(a._createdAt);
-        }).map((item, i) => 
-        <div className="bloglet" key={i}>
-            <h1 className="blogletheader">{item.title}</h1>
-            <h4 className="blogletdate">{item._createdAt.substring(0,10)}</h4>
-            <div className={"body"}>
-                <PortableText value={item.body}/>
-            </div>
-        </div>
-)}
+        }).map((item, i) => <Bloglet item={item} i={i}/>) : <h1>Trouble getting bloglist</h1>}
     </div>
-    <p>{props.url}</p>
     </>);
 }
