511061b9a8f9bf7368c6cdcf1e49b19c9054a85e
shimekukuri <jhutchinson192390@gmail.com>
2022-06-11 07:40:15 -0400

    Updates

--- src/componants/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import clsx from "clsx";
-
-export default function Button(props){
-    const {className, children, isClicked, ...rest} = props;
-    const classes = clsx({"btn": true}, className)
-
-    return(<><button className={classes} {...rest}>{children}</button></>)
-}
--- src/componants/GetHub.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import clsx from "clsx";
-
-export default function Gethub(props) {
-
-    return(<>
-        <h1>Gethub Test</h1>
-    </>);
-}
