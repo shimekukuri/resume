0766cc1a2a1237df61fb5e810849f57bdbe9f49f
shimekukuri <jhutchinson192390@gmail.com>
2022-06-14 14:39:07 -0400

    added a sudo loader to the twitter embed.
    added CSS animation for the loader

--- src/componants/Twitter.js
+++ src/componants/Twitter.js
@@ -1,13 +1,28 @@
-import React from "react";
-import {TwitterTimelineEmbed} from "react-twitter-embed"
+import React, { useState, useEffect } from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
-export default function Twitter(props) {    
+export default function Twitter(props) {
+  const [sudoLoad, setSudoLoad] = useState(true);
 
-    return (<>
-    <div className="twittercontainer">
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setSudoLoad(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="twittercontainer">
+        {sudoLoad && <div className="loader"></div>}
         <div className="twitterframe">
-            <TwitterTimelineEmbed sourceType="profile" screenName="shimeku" options={{height:(window.screen.height - 200), width:1000}}/>
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="shimeku"
+            options={{ height: window.screen.height - 200, width: 1000 }}
+          />
         </div>
-    </div>
-    </>);
+      </div>
+    </>
+  );
 }
