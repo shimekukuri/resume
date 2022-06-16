d231b5ccdeab468b2a784823f2e2c0e237715635
shimekukuri <jhutchinson192390@gmail.com>
2022-06-15 20:21:30 -0400

    General formating changes

--- src/componants/navbar.js
+++ src/componants/navbar.js
@@ -3,24 +3,38 @@ import clsx from "clsx";
 import Button from "./Button.js";
 
 export default function Navbar(props) {
-    const {className, navbarActive, onNavbarToggle, onNavbarSelection} = props;
-    const classes = clsx(className, {activate: navbarActive});
-    
-    const handleGitHubClick = () => {
-        window.open("https://github.com/shimekukuri");
-    };
+  const { className, navbarActive, onNavbarToggle, onNavbarSelection } = props;
+  const classes = clsx(className, { activate: navbarActive });
 
-    return (<>
-    <div id="navbar" className={classes}>
-        <button id="navbarexit" onClick={onNavbarToggle}>X</button>
+  const handleGitHubClick = () => {
+    window.open("https://github.com/shimekukuri");
+  };
+
+  return (
+    <>
+      <div id="navbar" className={classes}>
+        <button id="navbarexit" onClick={onNavbarToggle}>
+          X
+        </button>
         <h1 className="navbarheader">Menu</h1>
         <ul className="navList">
-            <li className="navbarlist"><Button onClick={() => onNavbarSelection(`twitter`)}>Twitter</Button></li>
-            <li className="navbarlist"><Button onClick={handleGitHubClick}>Gethub</Button></li>
-            <li className="navbarlist"><Button onClick={() => onNavbarSelection(`resume`)}>Resume</Button></li>
-            <li className="navbarlist"><Button onClick={() => onNavbarSelection('blog')}>Blog</Button></li>
+          <li className="navbarlist">
+            <Button onClick={() => onNavbarSelection(`twitter`)}>
+              Twitter
+            </Button>
+          </li>
+          <li className="navbarlist">
+            <Button onClick={handleGitHubClick}>Gethub</Button>
+          </li>
+          <li className="navbarlist">
+            <Button onClick={() => onNavbarSelection(`resume`)}>Resume</Button>
+          </li>
+          <li className="navbarlist">
+            <Button onClick={() => onNavbarSelection("blog")}>Blog</Button>
+          </li>
         </ul>
         <div className="footer">© Tyler James Hutchinson</div>
-    </div>
-    </>);
+      </div>
+    </>
+  );
 }
